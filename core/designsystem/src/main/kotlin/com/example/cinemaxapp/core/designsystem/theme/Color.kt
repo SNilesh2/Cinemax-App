7f6d0103309db2277e17ed66ea7bb196f4d6481d
nilesh <snileshofficial@gmail.com>
2026-07-31 00:23:40 +0530

    Add color palette to design system

---
+++ core/designsystem/src/main/kotlin/com/example/cinemaxapp/core/designsystem/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.cinemax.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary
+val Dark = Color(0xFF1F1D2B)
+val Soft = Color(0xFF252836)
+val BlueAccent = Color(0xFF12CDD9)
+
+// Secondary
+val Green = Color(0xFF22B07D)
+val Orange = Color(0xFFFF8700)
+val Red = Color(0xFFFB4141)
+
+// Text Colors
+val TextBlack = Color(0xFF171725)
+val DarkGrey = Color(0xFF696974)
+val Grey = Color(0xFF92929D)
+val WhiteGrey = Color(0xFFEBEBEF)
+val White = Color(0xFFFFFFFF)
+val LineDark = Color(0xFFEAEAEA)
